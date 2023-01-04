be9f1ec825253f8c5c2995e1112d68ed28b455fc
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2023-01-04 17:17:04 -0400

    Laboratorio de la Suma

---
+++ suma.js
@@ -0,0 +1,12 @@
+var a = 5
+var b = 2
+// let suma=a+b
+let resta=a-b
+let multiplicación=a*b
+let division=a/b
+let modulo = a % b
+console.log(`La suma de ${a} + ${b} = ${a+b}`);
+console.log(`La resta de ${a} - ${b} = ${a-b}`);
+console.log(`La multiplicación de ${a} * ${b} = ${multiplicación}`);
+console.log(`La division de ${a} / ${b} = ${division}`);
+console.log(`La division de ${a} % ${b} = ${modulo}`);
