67338d90351a2effe638d937666739b625c24ee8
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2023-01-13 10:11:25 -0400

    primer algoritmo

---
+++ algoritmoBásico.js
@@ -0,0 +1,8 @@
+//! Crea la secuencia de pasos en palabras que describe el algoritmo para
+//!  crear un documento HTML, usando console.log();
+console.log('ingresar al explorador de archivos');
+console.log('crear una carpeta con el nombre de nuestra preferncia');
+console.log('entrar a la carpeta que habíamos creado anteriormente');
+console.log('hacemos click derecho y escogemos la opción de abrir con visual studio code');
+console.log('una vez que se abra nuestro editor en nuestra carpeta creada anteriormente , debemos ir al panel izquierdo y hacemos click en el icono de new file ');
+console.log('escribimos index.html haciendo enfasis en el .html ya que es la extensión de nuestro archivo de html');
